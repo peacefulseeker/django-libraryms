982fce2c8234ec3d59dbd297ad8865fff074255b
Alexey Vorobyov <alexey.vorobyov.s@gmail.com>
2024-07-10 09:49:09 +0300

    redirect to admin

--- src/core/urls.py
+++ src/core/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls, name="admin"),
+    path('', RedirectView.as_view(pattern_name="admin:login")),
 ]
