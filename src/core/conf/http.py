c9ef9fe2f6dded4f93bf295230fd2cb26b52a62b
Alexey Vorobyov <alexey.vorobyov.s@gmail.com>
2024-08-01 16:52:20 +0300

    csrf settings for non debug mode

--- src/core/conf/http.py
+++ src/core/conf/http.py
@@ -3,7 +3,9 @@ from core.conf.environ import env
 if not env("DEBUG"):
     ALLOWED_HOSTS = env("ALLOWED_HOSTS").split(" ")
     CORS_ALLOWED_ORIGINS = env("CORS_ALLOWED_ORIGINS").split(" ")
+
+    # at this point CSRF only needed for admin actions
     CSRF_TRUSTED_ORIGINS = env("CSRF_TRUSTED_ORIGINS").split(" ")
-    print("CSRF_TRUSTED_ORIGINS", CSRF_TRUSTED_ORIGINS)
-    print("CORS_ALLOWED_ORIGINS", CORS_ALLOWED_ORIGINS)
-    print("ALLOWED_HOSTS", ALLOWED_HOSTS)
+    CSRF_COOKIE_HTTPONLY = True
+    CSRF_COOKIE_SECURE = True
+    print("NOT DEBUG!")
