b5cc76f82ca78a3962415ada388a8cb272642dec
Alexey Vorobyov <alexey.vorobyov.s@gmail.com>
2024-08-01 10:31:31 +0300

    remove cd

--- build.sh
+++ build.sh
@@ -13,6 +13,4 @@ poetry run python src/manage.py collectstatic --no-input
 # Apply any outstanding database migrations
 poetry run python src/manage.py migrate
 
-cd src/
-
 # python -m gunicorn -b localhost:6004 --env DEBUG=false core.wsgi:application
