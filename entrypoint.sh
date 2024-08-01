74c0847f93bcd0c07b024e170214577600fc6b01
Alexey Vorobyov <alexey.vorobyov.s@gmail.com>
2024-08-01 11:06:46 +0300

    dockerfile dev only for now

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,18 +3,7 @@
 # Exit on error
 set -o errexit
 
-echo "Applying migrations"
 python src/manage.py migrate
-
-if [ "$ENVIRONMENT" = "production" ]; then
-    echo "Running in production mode"
-    gunicorn -c gunicorn.conf.py
-elif [ "$ENVIRONMENT" = "development" ]; then
-    echo "Running in development mode"
-    python src/manage.py runserver 0.0.0.0:7070
-else
-    echo "Unknown environment. Exiting."
-    exit 1
-fi
+python src/manage.py runserver 0.0.0.0:7070
 
 exec "$@"
