aee178fff5c5ca52e94fe859005f0a29080ebec7
Alexey Vorobyov <alexey.vorobyov.s@gmail.com>
2024-08-02 18:47:11 +0300

    include other frontend routes listed

--- src/core/urls/__init__.py
+++ src/core/urls/__init__.py
@@ -1,18 +1,14 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from django.urls.conf import include
 from django.views.generic.base import TemplateView
 
 from core.views import app_view
 
-api = [
-    path("v1/", include("core.urls.api.v1")),
-]
-
 urlpatterns = [
-    path("api/", include(api), name="api"),
+    path("api/v1/", include("core.urls.api.v1"), name="api"),
     path("admin/", admin.site.urls, name="admin"),
     path("robots.txt", TemplateView.as_view(template_name="robots.txt", content_type="text/plain")),
 ]
@@ -20,9 +16,7 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-# important to put at very end to respect patterns above
 urlpatterns += [
     path("", app_view, name="app_home"),
-    path("login/", app_view, name="app_login"),
-    # re_path(r"^(?!\/static\/).*\/$", app_view, name="app_other"),
+    re_path("^(login|books|account)", app_view, name="app_routes"),
 ]
--- src/core/views.py
+++ src/core/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 
-def app_view(request):
+def app_view(request, *args, **kwargs):
     return render(request, "vue-index.html", context={})
