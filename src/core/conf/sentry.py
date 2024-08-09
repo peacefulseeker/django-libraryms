3a3e7585f8f3ed2b9d831482cac5c85a76b91b4f
Alexey Vorobyov <alexey.vorobyov.s@gmail.com>
2024-08-09 17:05:48 +0300

    Add sentry exception tracking
    should auto-capture unhandled exceptions both
    from main app and also from celery worker

---
+++ src/core/conf/sentry.py
@@ -0,0 +1,26 @@
+from core.conf.environ import env
+
+SENTRY_DSN = env("SENTRY_DSN", cast=str, default="")
+
+
+# if not env("DEBUG") and SENTRY_DSN:
+if SENTRY_DSN:
+    import sentry_sdk
+    from sentry_sdk.integrations.celery import CeleryIntegration
+    from sentry_sdk.integrations.django import DjangoIntegration
+
+    def strip_user_data(event, hint):
+        if event.get("user"):
+            user = event["user"]
+            event["user"] = {
+                "id": user.get("id"),
+            }
+        return event
+
+    sentry_sdk.init(
+        dsn=SENTRY_DSN,
+        integrations=[DjangoIntegration(), CeleryIntegration()],
+        traces_sample_rate=0.8,
+        send_default_pii=True,
+        before_send=strip_user_data,
+    )
