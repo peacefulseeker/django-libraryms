4dc5f281d7109c5a55800ca2d5ebda7bfe861b73
Alexey Vorobyov <alexey.vorobyov.s@gmail.com>
2024-08-09 17:06:17 +0300

    upd

--- src/core/conf/sentry.py
+++ src/core/conf/sentry.py
@@ -3,8 +3,7 @@ from core.conf.environ import env
 SENTRY_DSN = env("SENTRY_DSN", cast=str, default="")
 
 
-# if not env("DEBUG") and SENTRY_DSN:
-if SENTRY_DSN:
+if not env("DEBUG") and SENTRY_DSN:
     import sentry_sdk
     from sentry_sdk.integrations.celery import CeleryIntegration
     from sentry_sdk.integrations.django import DjangoIntegration
