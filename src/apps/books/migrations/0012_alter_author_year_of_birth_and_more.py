86ef8386867b00b39fb284d60b771736bce58042
Alexey Vorobyov <alexey.vorobyov.s@gmail.com>
2024-08-13 09:02:55 +0300

    format migration

--- src/apps/books/migrations/0012_alter_author_year_of_birth_and_more.py
+++ src/apps/books/migrations/0012_alter_author_year_of_birth_and_more.py
@@ -6,41 +6,44 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('books', '0011_alter_reservation_options'),
-        ('users', '0003_make_user_email_unique'),
+        ("books", "0011_alter_reservation_options"),
+        ("users", "0003_make_user_email_unique"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='author',
-            name='year_of_birth',
-            field=models.PositiveSmallIntegerField(blank=True, null=True, validators=[django.core.validators.MaxValueValidator(2024, message='Year of birth cannot be greater than current year')]),
+            model_name="author",
+            name="year_of_birth",
+            field=models.PositiveSmallIntegerField(
+                blank=True, null=True, validators=[django.core.validators.MaxValueValidator(2024, message="Year of birth cannot be greater than current year")]
+            ),
         ),
         migrations.AlterField(
-            model_name='author',
-            name='year_of_death',
-            field=models.PositiveSmallIntegerField(blank=True, null=True, validators=[django.core.validators.MaxValueValidator(2024, message='Year of birth cannot be greater than current year')]),
+            model_name="author",
+            name="year_of_death",
+            field=models.PositiveSmallIntegerField(
+                blank=True, null=True, validators=[django.core.validators.MaxValueValidator(2024, message="Year of birth cannot be greater than current year")]
+            ),
         ),
         migrations.AlterField(
-            model_name='book',
-            name='author',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='books', to='books.author'),
+            model_name="book",
+            name="author",
+            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name="books", to="books.author"),
         ),
         migrations.AlterField(
-            model_name='book',
-            name='publisher',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='books', to='books.publisher'),
+            model_name="book",
+            name="publisher",
+            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name="books", to="books.publisher"),
         ),
         migrations.AlterField(
-            model_name='order',
-            name='book',
-            field=models.ForeignKey(null=True, on_delete=django.db.models.deletion.SET_NULL, related_name='orders', to='books.book'),
+            model_name="order",
+            name="book",
+            field=models.ForeignKey(null=True, on_delete=django.db.models.deletion.SET_NULL, related_name="orders", to="books.book"),
         ),
         migrations.AlterField(
-            model_name='order',
-            name='member',
-            field=models.ForeignKey(null=True, on_delete=django.db.models.deletion.SET_NULL, related_name='orders', to='users.member'),
+            model_name="order",
+            name="member",
+            field=models.ForeignKey(null=True, on_delete=django.db.models.deletion.SET_NULL, related_name="orders", to="users.member"),
         ),
     ]
