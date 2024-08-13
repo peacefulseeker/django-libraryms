5e283546e2c9a17b89ab8af9e28f7e4f93efae20
Alexey Vorobyov <alexey.vorobyov.s@gmail.com>
2024-08-13 18:31:17 +0300

    move _create_tmp_index_template to conftest
    + autouse and trigger once only during a session

--- tests/core/test_views.py
+++ tests/core/test_views.py
@@ -1,23 +1,7 @@
-import os
 from unittest.mock import patch
 
 import pytest
 from rest_framework.status import HTTP_200_OK
-
-
-@pytest.fixture(autouse=True)
-def _create_tmp_index_template():
-    # setup
-    template_path = "src/core/templates/vue-index.html"
-    if os.path.exists(template_path):
-        yield
-        return
-
-    f = open(template_path, "w")
-    yield
-
-    # teardown
-    os.remove(f.name)
 
 
 @pytest.mark.parametrize(
