d03e6e964de4a5759369c03436408c5368e932a9
Alexey Vorobyov <alexey.vorobyov.s@gmail.com>
2024-08-23 17:32:57 +0300

    README update

--- scripts/build-frontend.sh
+++ scripts/build-frontend.sh
@@ -2,5 +2,6 @@ cd frontend
 pnpm install --frozen-lockfile
 pnpm run build
 
+mkdir -p ../src/core/assets/
 cp -r dist/ ../src/core/assets/frontend/
 cp ../src/core/assets/frontend/index.html ../src/core/templates/vue-index.html
