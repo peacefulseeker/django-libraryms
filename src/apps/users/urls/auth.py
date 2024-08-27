74328bf01c2427c4e1b5fe8c117a8c2b0a8b4ea9
Alexey Vorobyov <alexey.vorobyov.s@gmail.com>
2024-08-27 21:56:54 +0300

    password/reset-confirm

--- src/apps/users/urls/auth.py
+++ src/apps/users/urls/auth.py
@@ -17,5 +17,5 @@ urlpatterns = [
     path("me/", MemberProfileView.as_view(), name="member_profile"),
     path("password/change/", MemberPasswordChange.as_view(), name="member_password_change"),
     path("password/reset/", MemberPasswordReset.as_view(), name="member_password_reset"),
-    path("password/reset_confirm/", MemberPasswordResetConfirm.as_view(), name="member_password_reset_confirm"),
+    path("password/reset-confirm/", MemberPasswordResetConfirm.as_view(), name="member_password_reset_confirm"),
 ]
