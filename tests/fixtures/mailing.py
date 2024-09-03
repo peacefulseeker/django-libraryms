560da656b62ede8b4685534564c45b25b34042b5
Alexey Vorobyov <alexey.vorobyov.s@gmail.com>
2024-09-03 13:25:29 +0300

    move mailer mock to conftest
    it's simple enough at this point anyway

--- tests/fixtures/mailing.py
+++
@@ -1,8 +0,0 @@
-import pytest
-
-
-@pytest.fixture(autouse=True)
-def mock_mailer(mocker):
-    mocked = mocker.patch("core.tasks.Mailer")
-    mocked.return_value.send.return_value = 1
-    return mocked
